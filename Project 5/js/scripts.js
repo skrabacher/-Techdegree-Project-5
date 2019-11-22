acac00eeb2ae325e714e34894f11d5614ad3608d
skrabacher <37253871+skrabacher@users.noreply.github.com>
2019-11-22 12:26:14 -0800

    created card templates populated with data and displayed to the gallery

--- Project 5/js/scripts.js
+++ Project 5/js/scripts.js
@@ -2,13 +2,14 @@
  * Project 5 - Public API Requests for Employee Directory 
  * Sarah Krabacher */
 
+
 // ------------------------------------------
 //  FETCH FUNCTIONS
 // ------------------------------------------
 
   fetch('https://randomuser.me/api/?results=12') //fetches 12 random users
     .then(response => response.json()) //converts to json object array with 12 users
-    .then(data => console.log(data))
+    .then(data => generateEmployeeCard(data.results))
     .catch(error => console.log(error.message))
 
 
@@ -16,6 +17,24 @@
 //  HELPER FUNCTIONS
 // ------------------------------------------
 
+function generateEmployeeCard(userArray) { //parameter for this function must be an array
+  userArray.forEach(user => { //loops through each user in the array and places their data into the card template for html
+    console.log(user)
+    let cardTemplate= `<div class="card">
+                  <div class="card-img-container">
+                      <img class="card-img" src="${user.picture.medium}" alt="profile picture">
+                  </div>
+                  <div class="card-info-container">
+                      <h3 id="name" class="card-name cap">${user.name.first} ${user.name.last}</h3>
+                      <p class="card-text">${user.email}</p>
+                      <p class="card-text cap">${user.location.city}, ${user.location.state}</p>
+                  </div>`
+ $("#gallery").append(cardTemplate); //appends cardTemplate to the gallery div in the html file
+
+
+})
+
+};
 
 
     // <div id="gallery" class="gallery">
