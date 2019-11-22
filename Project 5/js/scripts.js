7e8555f79b0b6d54bba4780ae9670adb9b855784
skrabacher <37253871+skrabacher@users.noreply.github.com>
2019-11-22 13:21:14 -0800

    drafted click event listener for modal (not working yet)

--- Project 5/js/scripts.js
+++ Project 5/js/scripts.js
@@ -2,14 +2,13 @@
  * Project 5 - Public API Requests for Employee Directory 
  * Sarah Krabacher */
 
-
 // ------------------------------------------
 //  FETCH FUNCTIONS
 // ------------------------------------------
 
   fetch('https://randomuser.me/api/?results=12') //fetches 12 random users
     .then(response => response.json()) //converts to json object array with 12 users
-    .then(data => generateEmployeeCard(data.results))
+    .then(data => generateEmployeeCards(data.results))
     .catch(error => console.log(error.message))
 
 
@@ -17,7 +16,7 @@
 //  HELPER FUNCTIONS
 // ------------------------------------------
 
-function generateEmployeeCard(userArray) { //parameter for this function must be an array
+function generateEmployeeCards(userArray) { //parameter for this function must be an array
   userArray.forEach(user => { //loops through each user in the array and places their data into the card template for html
     console.log(user)
     let cardTemplate= `<div class="card">
@@ -36,29 +35,34 @@ function generateEmployeeCard(userArray) { //parameter for this function must be
 
 };
 
+function generateEmployeeModal(userArray) {
+ userArray.forEach(user => {
+  console.log(user)
+    let employeeModal= `<div class="modal-container">
+    <div class="modal">
+        <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+        <div class="modal-info-container">
+            <img class="modal-img" src="${user.picture.large}" alt="profile picture">
+            <h3 id="name" class="modal-name cap">${user.name.first} ${user.name.last}</h3>
+            <p class="modal-text">${user.email}</p>
+            <p class="modal-text cap">${user.location.city}</p>
+            <hr>
+            <p class="modal-text">${user.phone}</p>
+            <p class="modal-text">${user.location.street.number} ${user.location.street.name}, ${user.location.city}, ${user.location.state} ${user.location.postcode}</p>
+            <p class="modal-text">Birthday: ${user.dob.date} <br>***NEED TO REFORMAT to MM/DD/YYYY***</p>
+        </div>
+    </div>`
+    $("body").append(employeeModal); //appends employeeModal to the body of the html file
 
-    // <div id="gallery" class="gallery">
-    //         <!-- ======================
-    //             Gallery markup: 
-
-    //             You can use the commented out markup below as a template
-    //             for each of your Gallery items, but you must use JS to 
-    //             create and append them to the `gallery` div.
-
-    //             IMPORTANT: Altering the arrangement of the markup and the 
-    //             attributes used may break the styles or functionality.
-
-    //             <div class="card">
-    //                 <div class="card-img-container">
-    //                     <img class="card-img" src="https://placehold.it/90x90" alt="profile picture">
-    //                 </div>
-    //                 <div class="card-info-container">
-    //                     <h3 id="name" class="card-name cap">first last</h3>
-    //                     <p class="card-text">email</p>
-    //                     <p class="card-text cap">city, state</p>
-    //                 </div>
-    //             </div>
+ })
+};
 
 // ------------------------------------------
 //  EVENT LISTENERS
 // ------------------------------------------
+const card = document.querySelectorAll('.card'); //***try using click events from Proj 3 with jquery here */
+console.log(card);
+card.addEventListener('click', console.log("generateEmployeeModal"));//click event handler to open employee modal
+
+const xButton = document.querySelector('.modal-close-button');
+xButton.addEventListener('click', generateEmployeeCards);//click event handler to close employee modal
